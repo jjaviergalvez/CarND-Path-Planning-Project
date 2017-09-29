dcfe469cff39c9a38b328f1b0bf6868a8d774584
jjaviergalvez <jjaviergalvez@hotmail.com>
2017-09-28 23:52:16 -0600

    feat: stay in a line using splines. code from Walkthrough video

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@
 #include "Eigen-3.3/Eigen/Core"
 #include "Eigen-3.3/Eigen/QR"
 #include "json.hpp"
+#include "spline.h"
 
 using namespace std;
 
@@ -197,8 +198,14 @@ int main() {
   	map_waypoints_dy.push_back(d_y);
   }
 
+  // start in lane 1
+  int lane = 1;
 
-  h.onMessage([&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,&map_waypoints_dx,&map_waypoints_dy](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
+  // Have a reference velocity to target
+  double ref_vel = 49.5;
+
+
+  h.onMessage([&lane,&ref_vel,&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,&map_waypoints_dx,&map_waypoints_dy](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
                      uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
     // The 4 signifies a websocket message
@@ -243,6 +250,121 @@ int main() {
 
           	// TODO: define a path made up of (x,y) points that the car will visit sequentially every .02 seconds
           	
+
+          	int prev_size = previous_path_x.size();
+
+          	// Create a list of widely spaced (x,y) waypoints, evenly spaced at 30m
+          	// Later we will interpolate these waypoints with a spline and fill it in with more points that control speed
+          	vector<double> ptsx;
+          	vector<double> ptsy;
+
+          	// reference x,y yaw states
+          	// either we will reference the satarting point as where the car is or at the previous paths end point
+          	double ref_x = car_x;
+          	double ref_y = car_y;
+          	double ref_yaw = deg2rad(car_yaw);
+
+
+          	// if previous path is almost empty, use the car as startinf reference
+          	if(prev_size < 2){
+          		// Use two points that make the path tangent to the car
+          		double prev_car_x = car_x - cos(car_yaw);
+          		double prev_car_y = car_y - sin(car_yaw);
+
+          		ptsx.push_back(prev_car_x);
+          		ptsx.push_back(car_x);
+          		
+          		ptsy.push_back(prev_car_y);
+          		ptsy.push_back(car_y);
+
+          	}else{
+          		// Use the previous path's end point as starting reference 
+
+          		//redefine reference state as previous path end point
+          		ref_x = previous_path_x[prev_size-1];
+          		ref_y = previous_path_y[prev_size-1];
+
+          		double ref_x_prev = previous_path_x[prev_size-2];
+          		double ref_y_prev = previous_path_y[prev_size-2];
+          		ref_yaw = atan2(ref_y - ref_y_prev, ref_x - ref_x_prev);
+
+          		// Use the two points that make the path tangent to the previous path's end point
+          		ptsx.push_back(ref_x_prev);
+          		ptsx.push_back(ref_x);
+
+          		ptsy.push_back(ref_y_prev);
+          		ptsy.push_back(ref_y);
+
+          	}
+
+          	// In Frenet add evenly 30m spaced points ahead of the starting reference
+          	vector<double> next_wp0 = getXY(car_s+30, (2+4*lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+          	vector<double> next_wp1 = getXY(car_s+60, (2+4*lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+          	vector<double> next_wp2 = getXY(car_s+90, (2+4*lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+
+          	ptsx.push_back(next_wp0[0]);
+          	ptsx.push_back(next_wp1[0]);
+          	ptsx.push_back(next_wp2[0]);
+
+          	ptsy.push_back(next_wp0[1]);
+          	ptsy.push_back(next_wp1[1]);
+          	ptsy.push_back(next_wp2[1]);
+
+          	for (int i = 0; i < ptsx.size(); i++){
+          		// Shift car reference angle to 0 
+          		double shift_x = ptsx[i] - ref_x;
+          		double shift_y = ptsy[i] - ref_y;
+
+          		ptsx[i] = shift_x*cos(0-ref_yaw) - shift_y*sin(0-ref_yaw);
+          		ptsy[i] = shift_x*sin(0-ref_yaw) + shift_y*cos(0-ref_yaw);
+
+          	}
+
+          	// create a spline
+          	tk::spline s;
+
+          	// set (x,y) points to the spline
+          	s.set_points(ptsx,ptsy);
+
+          	// Define the actual (x,y) points we will use for the planner
+          	// Start with all od the previous path points from last time
+          	for(int i = 0; i < previous_path_x.size(); i++){
+          		next_x_vals.push_back(previous_path_x[i]);
+          		next_y_vals.push_back(previous_path_y[i]);
+          	}
+
+          	//Calculate how to breack up spline points so that we travel at pur desired reference velocity
+          	double target_x = 30.0;
+          	double target_y = s(target_x);
+          	//double target_dist = sqrt(target_x*target_x + target_y*target_y);
+          	double target_dist = sqrt((target_x)*(target_x) + (target_y)*(target_y)); // parentesis
+
+          	double x_add_on = 0; 
+
+          	// Fill up the rest of our path planner after filling it with previous points, here we eill always output 50 points
+          	for(int i = 1; i <= 50-previous_path_x.size(); i++){
+          		double N = target_dist / (0.02*ref_vel/2.24);
+          		double x_point = x_add_on + target_x/N;
+          		double y_point = s(x_point);
+
+          		x_add_on = x_point;
+
+          		double x_ref = x_point;
+          		double y_ref = y_point;
+
+          		// rotate back to normal after rotating it earlier
+          		x_point = x_ref*cos(ref_yaw) - y_ref*sin(ref_yaw);
+          		y_point = x_ref*sin(ref_yaw) + y_ref*cos(ref_yaw);
+
+          		x_point += ref_x;
+          		y_point += ref_y;
+
+          		next_x_vals.push_back(x_point);
+          		next_y_vals.push_back(y_point);
+
+          	}
+
+/*
 		    double dist_inc = 50.0/112;
 		    for(int i = 0; i < 50; i++)
 		    {
@@ -256,8 +378,7 @@ int main() {
 		        // next_x_vals.push_back(car_x+(dist_inc*i)*cos(deg2rad(car_yaw)));
 		        // next_y_vals.push_back(car_y+(dist_inc*i)*sin(deg2rad(car_yaw)));		        
 		    }
-
-
+*/
 		    // TODO END
 		    
           	msgJson["next_x"] = next_x_vals;
